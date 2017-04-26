685c244edd253abfa16892d11ebbc7a07bc15f20
James Gillmore <james@faceyspacey.com>
2017-04-26 04:46:06 -0700

    chore($travis): add semantic-release + travis configuration

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,21 +1,15 @@
-
 module.exports = {
   parser: 'babel-eslint',
   parserOptions: {
     ecmaFeatures: {
       generators: true,
-      experimentalObjectRestSpread: true,
+      experimentalObjectRestSpread: true
     },
     sourceType: 'module',
     allowImportExportEverywhere: false
   },
-  plugins: [
-    'flowtype'
-  ],
-  extends: [
-    'airbnb',
-    'plugin:flowtype/recommended'
-  ],
+  plugins: ['flowtype'],
+  extends: ['airbnb', 'plugin:flowtype/recommended'],
   settings: {
     flowtype: {
       onlyFilesWithFlowAnnotation: true
@@ -40,31 +34,25 @@ module.exports = {
       extensions: ['.js', '.css', '.json', '.styl']
     }
   },
-  'import/extensions': [
-    '.js',
-  ],
-  'import/ignore': [
-    'node_modules',
-    'flow-typed',
-    '\\.(css|styl|svg|json)$',
-  ],
+  'import/extensions': ['.js'],
+  'import/ignore': ['node_modules', 'flow-typed', '\\.(css|styl|svg|json)$'],
   rules: {
     'no-shadow': 0,
     'comma-dangle': 0,
     'no-use-before-define': 0,
     'no-param-reassign': 0,
-    'camelcase': 1,
+    camelcase: 1,
     'prefer-template': 1,
     'react/no-array-index-key': 1,
     'global-require': 1,
     'react/jsx-indent': 1,
     'dot-notation': 1,
     'import/no-named-default': 1,
-    'semi': [2, 'never'],
+    semi: [2, 'never'],
     'flowtype/semi': [2, 'never'],
     'jsx-quotes': [2, 'prefer-single'],
     'react/jsx-filename-extension': [2, { extensions: ['.jsx', '.js'] }],
     'spaced-comment': [2, 'always', { markers: ['?'] }],
-    'arrow-parens': [2, 'as-needed', { requireForBlockBody: false }],
+    'arrow-parens': [2, 'as-needed', { requireForBlockBody: false }]
   }
 }
