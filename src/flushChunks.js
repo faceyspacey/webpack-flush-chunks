90daf3e212810a1f1261a5d5059c6343d72b72d3
James Gillmore <james@faceyspacey.com>
2017-08-18 07:25:23 -0700

    fix($eslint): space infix operator

--- src/flushChunks.js
+++ src/flushChunks.js
@@ -195,7 +195,7 @@ const filesFromChunks = (
   checkChunkNames?: boolean
 ): Files => {
   const hasChunk = entry => {
-    const result = !!(assets[entry] || assets[entry+'-'])
+    const result = !!(assets[entry] || assets[entry + '-'])
     if (!result && checkChunkNames) {
       console.warn(`[FLUSH CHUNKS]: Unable to find ${entry} in Webpack chunks. Please check usage of Babel plugin.`)
     }
@@ -203,7 +203,7 @@ const filesFromChunks = (
     return result
   }
 
-  const entryToFiles = entry => assets[entry]|| assets[entry+'-']
+  const entryToFiles = entry => assets[entry]|| assets[entry + '-']
 
   return [].concat(...chunkNames.filter(hasChunk).map(entryToFiles))
 }
