9172af3c9ac19ed2d2a5287d84c4ced7ff5878b0
bernardosunderhus <bernardosunderhus@conexops.com.br>
2017-09-01 08:01:01 -0300

    refactor(re-arranging files to avoid unecessary react dependency in most of the library): moved all
    the only method that relies on react are the ones provided by the createApiWithCss and the only
    method that depends on createApiWithCss is the flushChunks. By separating flushChunks from the rest
    of the methods I'm making this library a little bit more independent and more  like a universal
    solution to the chunks problems
    
    26

---
+++ __tests__/flushFiles.test.js
@@ -0,0 +1,177 @@
+// @noflow
+import {
+  flushFilesPure,
+  flushBabel,
+  flushWebpack,
+  createFilesByPath,
+  createFilesByModuleId,
+  isUnique,
+  normalizePath,
+  concatFilesAtKeys,
+  filesFromChunks
+} from '../src/flushFiles'
+
+import {
+  stats,
+  rootDir,
+  babelFilePaths,
+  webpackModuleIds,
+  chunkNames
+} from '../__fixtures__/stats'
+
+/** PUBLIC API */
+describe('flushFiles() called as a pure function', () => {
+  it('babel - moduleIds', () => {
+    const files = flushFilesPure(stats, false, {
+      moduleIds: babelFilePaths,
+      rootDir
+    }) /*? */
+
+    expect(files).toMatchSnapshot()
+  })
+
+  it('webpack - moduleIds', () => {
+    const files = flushFilesPure(stats, true, {
+      moduleIds: webpackModuleIds
+    }) /*? */
+
+    expect(files).toMatchSnapshot()
+  })
+
+  it('babel - chunkNames', () => {
+    const files = flushFilesPure(stats, false, {
+      chunkNames,
+      rootDir
+    }) /*? */
+
+    expect(files).toMatchSnapshot()
+  })
+
+  it('webpack - chunkNames', () => {
+    const files = flushFilesPure(stats, true, {
+      chunkNames
+    }) /*? */
+
+    expect(files).toMatchSnapshot()
+  })
+
+  it('filter: by string (file extension)', () => {
+    const files = flushFilesPure(stats, true, {
+      chunkNames,
+      filter: 'css'
+    }) /*? */
+
+    expect(files).toMatchSnapshot()
+  })
+
+  it('filter: by function', () => {
+    const files = flushFilesPure(stats, true, {
+      chunkNames,
+      filter: file => /\.css$/.test(file)
+    }) /*? */
+
+    expect(files).toMatchSnapshot()
+  })
+
+  it('filter: by regex', () => {
+    const files = flushFilesPure(stats, true, {
+      chunkNames,
+      filter: /\.css$/
+    }) /*? */
+
+    expect(files).toMatchSnapshot()
+  })
+})
+
+describe('unit tests', () => {
+  /** BABEL VS. WEBPACK FLUSHING */
+
+  test('flushBabel()', () => {
+    const files = flushBabel(babelFilePaths, stats, rootDir) /*? */
+    const allFiles = stats.chunks[0].files.concat(
+      stats.chunks[1].files,
+      stats.chunks[2].files
+    )
+    expect(files).toEqual(allFiles)
+  })
+
+  test('flushWebpack()', () => {
+    const files = flushWebpack(webpackModuleIds, stats) /*? */
+    const allFiles = stats.chunks[0].files.concat(
+      stats.chunks[1].files,
+      stats.chunks[2].files
+    )
+    expect(files).toEqual(allFiles)
+  })
+
+  test('flushBabel() throws with no rootDir argument', () => {
+    const flush = () => flushBabel(babelFilePaths, stats) /*? */
+    expect(flush).toThrow()
+  })
+
+  /** CREATE FILES MAP */
+
+  test('createFilesByPath()', () => {
+    const filesByPath = createFilesByPath(stats) /*? */
+
+    expect(Object.keys(filesByPath)).toEqual(babelFilePaths)
+
+    const files = stats.chunks[0].files
+    expect(filesByPath['./src/Components/Example.js']).toEqual(files)
+    expect(filesByPath['./src/Components/Baz.js']).toEqual([]) // test against arrays of undefined
+
+    expect(filesByPath).toMatchSnapshot()
+  })
+
+  test('createFilesByModuleId()', () => {
+    const filesByPath = createFilesByModuleId(stats) /*? */
+
+    expect(Object.keys(filesByPath)).toEqual(webpackModuleIds)
+
+    const files = stats.chunks[0].files
+    expect(filesByPath.qwer).toEqual(files)
+    expect(filesByPath.fgij).toEqual([]) // test against arrays of undefined
+
+    expect(filesByPath).toMatchSnapshot()
+  })
+
+  /** HELPERS */
+
+  test('isUnique()', () => {
+    let filtered = [1, 2, 2].filter(isUnique)
+    expect(filtered).toEqual([1, 2])
+
+    filtered = [1, 2, 3].filter(isUnique)
+    expect(filtered).toEqual([1, 2, 3])
+  })
+
+  test('normalizePath()', () => {
+    const path = '/Users/jamesgillmore/App/src/Components/Example.js'
+    const normalizedPath = normalizePath(path, rootDir)
+
+    expect(normalizedPath).toEqual('./src/Components/Example.js')
+  })
+
+  test('concatFilesAtKeys()', () => {
+    const filesMap = {
+      './src/Components/Example.js': ['0.js', '0.css'],
+      './src/Components/Foo.js': ['1.js', '1.css'],
+      './src/Components/Bar.js': ['2.js', '2.css']
+    }
+    const paths = ['./src/Components/Example.js', './src/Components/Bar.js']
+    const files = concatFilesAtKeys(filesMap, paths)
+
+    expect(files).toEqual(['0.js', '0.css', '2.js', '2.css'])
+  })
+
+  test('filesFromChunks()', () => {
+    const entryNames = ['bootstrap', 'vendor', 'main']
+    const assetsByChunkName = {
+      bootstrap: ['bootstrap.js'],
+      main: ['main.js', 'main.css']
+    }
+    const outputFiles = filesFromChunks(entryNames, assetsByChunkName)
+
+    expect(outputFiles).toEqual(['bootstrap.js', 'main.js', 'main.css'])
+  })
+})
--- src/flushChunks.js
+++ src/flushChunks.js
@@ -1,6 +1,7 @@
 // @flow
 import type { Api, CssChunksHash } from './createApiWithCss'
 import createApiWithCss from './createApiWithCss'
+import { filesFromChunks, flush } from './flushFiles'
 
 declare function __webpack_require__(id: string): any
 
@@ -37,16 +38,6 @@ type Options = {
   outputPath?: string
 }
 
-type Options2 = {
-  moduleIds?: Files,
-  chunkNames?: Files,
-  rootDir?: string,
-  filter?: string | Function | RegExp
-}
-
-let filesByPath = null
-let filesByModuleId = null
-
 const IS_WEBPACK = typeof __webpack_require__ !== 'undefined'
 const IS_TEST = process.env.NODE_ENV === 'test'
 const defaults = {
@@ -82,136 +73,8 @@ const flushChunks = (stats: Stats, isWebpack: boolean, opts: Options = {}) => {
   )
 }
 
-const flushFiles = (stats: Stats, opts: Options2) =>
-  flushFilesPure(stats, IS_WEBPACK, opts)
-
-const flushFilesPure = (stats: Stats, isWebpack: boolean, opts: Options2) => {
-  const files = opts.chunkNames
-    ? filesFromChunks(opts.chunkNames, stats.assetsByChunkName)
-    : flush(opts.moduleIds || [], stats, opts.rootDir, isWebpack)
-
-  const filter = opts.filter
-
-  if (filter) {
-    if (typeof filter === 'function') {
-      return files.filter(filter)
-    }
-
-    const regex = filter instanceof RegExp ? filter : new RegExp(`.${filter}$`)
-    return files.filter(file => regex.test(file))
-  }
-
-  return files
-}
-
-/** BABEL VS. WEBPACK FLUSHING */
-
-const flush = (
-  moduleIds: Files,
-  stats: Stats,
-  rootDir: ?string,
-  isWebpack: boolean
-) =>
-  (!isWebpack
-    ? flushBabel(moduleIds, stats, rootDir).filter(isUnique)
-    : flushWebpack(moduleIds, stats).filter(isUnique))
-
-const flushBabel = (paths: Files, stats: Stats, rootDir: ?string): Files => {
-  if (!rootDir) {
-    throw new Error(
-      `No \`rootDir\` was provided as an option to \`flushChunks\`.
-      Please provide one so modules rendered server-side can be
-      paired to their webpack equivalents client-side, and their
-      corresponding chunks.`
-    )
-  }
-
-  const dir = rootDir // satisfy flow
-
-  filesByPath = filesByPath && !IS_TEST
-    ? filesByPath // cached
-    : createFilesByPath(stats)
-
-  return concatFilesAtKeys(filesByPath, paths.map(p => normalizePath(p, dir)))
-}
-
-const flushWebpack = (ids: Files, stats: Stats): Files => {
-  filesByModuleId = filesByModuleId && !IS_TEST
-    ? filesByModuleId // cached
-    : createFilesByModuleId(stats)
-
-  return concatFilesAtKeys(filesByModuleId, ids)
-}
-
-/** CREATE FILES MAP */
-
-const createFilesByPath = ({ chunks, modules }: Stats): FilesMap => {
-  const filesByChunk = chunks.reduce((chunks, chunk) => {
-    chunks[chunk.id] = chunk.files
-    return chunks
-  }, {})
-
-  return modules.reduce((filesByPath, module) => {
-    const filePath = module.name
-    const files = concatFilesAtKeys(filesByChunk, module.chunks)
-
-    filesByPath[filePath] = files.filter(isUnique)
-    return filesByPath
-  }, {})
-}
-
-const createFilesByModuleId = (stats: Stats): FilesMap => {
-  const filesByPath = createFilesByPath(stats)
-
-  return stats.modules.reduce((filesByModuleId, module) => {
-    const filePath = module.name
-    const id = module.id
-
-    filesByModuleId[id] = filesByPath[filePath]
-    return filesByModuleId
-  }, {})
-}
-
-/** HELPERS */
-
-const isUnique = (v: string, i: number, self: Files): boolean =>
-  self.indexOf(v) === i
-
-const normalizePath = (path: string, rootDir: string): string =>
-  `${path.replace(rootDir, '.').replace(/\.js$/, '')}.js`
-
-const concatFilesAtKeys = (
-  inputFilesMap: FilesMap,
-  pathsOrIdsOrChunks: Array<any>
-): Files =>
-  pathsOrIdsOrChunks.reduce(
-    (files, key) => files.concat(inputFilesMap[key] || []),
-    []
-  )
-
-const filesFromChunks = (
-  chunkNames: Files,
-  assets: FilesMap,
-  checkChunkNames?: boolean
-): Files => {
-  const hasChunk = entry => {
-    const result = !!(assets[entry] || assets[entry + '-'])
-    if (!result && checkChunkNames) {
-      console.warn(`[FLUSH CHUNKS]: Unable to find ${entry} in Webpack chunks. Please check usage of Babel plugin.`)
-    }
-
-    return result
-  }
-
-  const entryToFiles = entry => assets[entry] || assets[entry + '-']
-
-  return [].concat(...chunkNames.filter(hasChunk).map(entryToFiles))
-}
-
-/** EXPORTS FOR TESTS */
-
+export { flushChunks }
 export {
-  flushChunks,
   flushFiles,
   flushFilesPure,
   flush,
@@ -223,4 +86,4 @@ export {
   normalizePath,
   concatFilesAtKeys,
   filesFromChunks
-}
+} from './flushFiles'
