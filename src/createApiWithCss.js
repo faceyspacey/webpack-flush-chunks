e368b71e1428af0ffe5328cbfc7a13d2ac4e2ebe
bernardosunderhus <bernardosunderhus@conexops.com.br>
2017-09-05 11:52:05 -0300

    Remove the StatelessComponent React type

--- src/createApiWithCss.js
+++ src/createApiWithCss.js
@@ -8,7 +8,7 @@ export type CssChunksHash = {
   [key: string]: string
 }
 
-type StatelessComponent = () => React.Element<*>
+type StatelessComponent = Object
 type ObjectString = {
   toString: () => string
 }
