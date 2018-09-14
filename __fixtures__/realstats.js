32bc2555ab56c4e7a3bc389ea2df373532ce6896
re6exp <re6exp@gmail.com>
2018-09-14 20:15:40 +0300

    Beginning

---
+++ __fixtures__/realstats.js
@@ -0,0 +1,423 @@
+const webpackStats = {
+  errors: [],
+  warnings: [],
+  version: '4.17.1',
+  hash: '71a99bf87aa88adb9ed8',
+  time: 10788,
+  builtAt: 1536834018822,
+  publicPath: '/',
+  outputPath: '/home/anton/Documents/workspace-sts/nodejs-spring-cloud-app-boilerplate/public',
+  assetsByChunkName: {
+    'components/App-App-test': [
+      'static/js/components/App-App-test.js',
+      'static/js/components/App-App-test.js.map'
+    ],
+    'components/Galleries': [
+      'static/js/components/Galleries.js',
+      'static/js/components/Galleries.js.map'
+    ],
+    'components/Home': [
+      'static/css/components/Home.css',
+      'static/js/components/Home.js',
+      'static/css/components/Home.css.map',
+      'static/js/components/Home.js.map'
+    ],
+    'components/Home-api': [
+      'static/js/components/Home-api.js',
+      'static/js/components/Home-api.js.map'
+    ],
+    'components/Home-home-scss': [
+      'static/css/components/Home-home-scss.css',
+      'static/js/components/Home-home-scss.js',
+      'static/css/components/Home-home-scss.css.map',
+      'static/js/components/Home-home-scss.js.map'
+    ],
+    'components/Pictures': [
+      'static/js/components/Pictures.js',
+      'static/js/components/Pictures.js.map'
+    ],
+    main: [
+      'static/css/main.css',
+      'static/js/main.js',
+      'static/css/main.css.map',
+      'static/js/main.js.map'
+    ]
+  },
+  assets: [
+    {
+      name: 'static/js/components/App-App-test.js.map',
+      size: 472,
+      chunks: ['components/App-App-test'],
+      chunkNames: ['components/App-App-test'],
+      emitted: true
+    },
+    {
+      name: 'static/js/components/App-App-test.js',
+      size: 677,
+      chunks: ['components/App-App-test'],
+      chunkNames: ['components/App-App-test'],
+      emitted: true
+    },
+    {
+      name: 'static/css/components/Home.css',
+      size: 129,
+      chunks: ['components/Home'],
+      chunkNames: ['components/Home'],
+      emitted: true
+    },
+    {
+      name: 'static/js/components/Home.js',
+      size: 7825,
+      chunks: ['components/Home'],
+      chunkNames: ['components/Home'],
+      emitted: true
+    },
+    {
+      name: 'static/js/components/Home-api.js',
+      size: 400,
+      chunks: ['components/Home-api'],
+      chunkNames: ['components/Home-api'],
+      emitted: true
+    },
+    {
+      name: 'static/css/components/Home-home-scss.css',
+      size: 139,
+      chunks: ['components/Home-home-scss'],
+      chunkNames: ['components/Home-home-scss'],
+      emitted: true
+    },
+    {
+      name: 'static/js/components/Home-home-scss.js',
+      size: 886,
+      chunks: ['components/Home-home-scss'],
+      chunkNames: ['components/Home-home-scss'],
+      emitted: true
+    },
+    {
+      name: 'static/js/components/Pictures.js',
+      size: 5604,
+      chunks: ['components/Pictures'],
+      chunkNames: ['components/Pictures'],
+      emitted: true
+    },
+    {
+      name: 'static/css/main.css',
+      size: 987,
+      chunks: ['main'],
+      chunkNames: ['main'],
+      emitted: true
+    },
+    {
+      name: 'static/js/main.js',
+      size: 2392449,
+      chunks: ['main'],
+      chunkNames: ['main'],
+      emitted: true
+    },
+    {
+      name: 'static/js/components/Galleries.js',
+      size: 7262,
+      chunks: ['components/Galleries'],
+      chunkNames: ['components/Galleries'],
+      emitted: true
+    },
+    {
+      name: 'static/js/components/Galleries.js.map',
+      size: 2633,
+      chunks: ['components/Galleries'],
+      chunkNames: ['components/Galleries'],
+      emitted: true
+    },
+    {
+      name: 'static/css/components/Home.css.map',
+      size: 296,
+      chunks: ['components/Home'],
+      chunkNames: ['components/Home'],
+      emitted: true
+    },
+    {
+      name: 'static/js/components/Home.js.map',
+      size: 2802,
+      chunks: ['components/Home'],
+      chunkNames: ['components/Home'],
+      emitted: true
+    },
+    {
+      name: 'static/js/components/Home-api.js.map',
+      size: 109,
+      chunks: ['components/Home-api'],
+      chunkNames: ['components/Home-api'],
+      emitted: true
+    },
+    {
+      name: 'static/css/components/Home-home-scss.css.map',
+      size: 306,
+      chunks: ['components/Home-home-scss'],
+      chunkNames: ['components/Home-home-scss'],
+      emitted: true
+    },
+    {
+      name: 'static/js/components/Home-home-scss.js.map',
+      size: 636,
+      chunks: ['components/Home-home-scss'],
+      chunkNames: ['components/Home-home-scss'],
+      emitted: true
+    },
+    {
+      name: 'static/js/components/Pictures.js.map',
+      size: 1466,
+      chunks: ['components/Pictures'],
+      chunkNames: ['components/Pictures'],
+      emitted: true
+    },
+    {
+      name: 'static/css/main.css.map',
+      size: 1411,
+      chunks: ['main'],
+      chunkNames: ['main'],
+      emitted: true
+    },
+    {
+      name: 'static/js/main.js.map',
+      size: 2312660,
+      chunks: ['main'],
+      chunkNames: ['main'],
+      emitted: true
+    }
+  ],
+  filteredAssets: 0,
+  entrypoints: {
+    main: {
+      chunks: ['main'],
+      assets: [
+        'static/css/main.css',
+        'static/js/main.js',
+        'static/css/main.css.map',
+        'static/js/main.js.map'
+      ],
+      children: {},
+      childAssets: {}
+    }
+  },
+  namedChunkGroups: {
+    main: {
+      chunks: ['main'],
+      assets: [
+        'static/css/main.css',
+        'static/js/main.js',
+        'static/css/main.css.map',
+        'static/js/main.js.map'
+      ],
+      children: {},
+      childAssets: {}
+    },
+    'components/App-App-scss': {
+      chunks: [],
+      assets: [],
+      children: {},
+      childAssets: {}
+    },
+    'components/App-App-test': {
+      chunks: ['components/App-App-test'],
+      assets: [
+        'static/js/components/App-App-test.js',
+        'static/js/components/App-App-test.js.map'
+      ],
+      children: {},
+      childAssets: {}
+    },
+    'components/App-App-test-js': {
+      chunks: ['components/App-App-test'],
+      assets: [
+        'static/js/components/App-App-test.js',
+        'static/js/components/App-App-test.js.map'
+      ],
+      children: {},
+      childAssets: {}
+    },
+    'components/App': {
+      chunks: [],
+      assets: [],
+      children: {},
+      childAssets: {}
+    },
+    'components/App-index': {
+      chunks: [],
+      assets: [],
+      children: {},
+      childAssets: {}
+    },
+    'components/App-index-js': {
+      chunks: [],
+      assets: [],
+      children: {},
+      childAssets: {}
+    },
+    'components/Galleries-ducks': {
+      chunks: [],
+      assets: [],
+      children: {},
+      childAssets: {}
+    },
+    'components/Galleries-ducks-js': {
+      chunks: [],
+      assets: [],
+      children: {},
+      childAssets: {}
+    },
+    'components/Galleries': {
+      chunks: ['components/Galleries'],
+      assets: [
+        'static/js/components/Galleries.js',
+        'static/js/components/Galleries.js.map'
+      ],
+      children: {},
+      childAssets: {}
+    },
+    'components/Galleries-index': {
+      chunks: ['components/Galleries'],
+      assets: [
+        'static/js/components/Galleries.js',
+        'static/js/components/Galleries.js.map'
+      ],
+      children: {},
+      childAssets: {}
+    },
+    'components/Galleries-index-js': {
+      chunks: ['components/Galleries'],
+      assets: [
+        'static/js/components/Galleries.js',
+        'static/js/components/Galleries.js.map'
+      ],
+      children: {},
+      childAssets: {}
+    },
+    'components/Home-api': {
+      chunks: ['components/Home-api'],
+      assets: [
+        'static/js/components/Home-api.js',
+        'static/js/components/Home-api.js.map'
+      ],
+      children: {},
+      childAssets: {}
+    },
+    'components/Home-api-js': {
+      chunks: ['components/Home-api'],
+      assets: [
+        'static/js/components/Home-api.js',
+        'static/js/components/Home-api.js.map'
+      ],
+      children: {},
+      childAssets: {}
+    },
+    'components/Home-ducks': {
+      chunks: [],
+      assets: [],
+      children: {},
+      childAssets: {}
+    },
+    'components/Home-ducks-js': {
+      chunks: [],
+      assets: [],
+      children: {},
+      childAssets: {}
+    },
+    'components/Home-home-scss': {
+      chunks: ['components/Home-home-scss'],
+      assets: [
+        'static/css/components/Home-home-scss.css',
+        'static/js/components/Home-home-scss.js',
+        'static/css/components/Home-home-scss.css.map',
+        'static/js/components/Home-home-scss.js.map'
+      ],
+      children: {},
+      childAssets: {}
+    },
+    'components/Home': {
+      chunks: ['components/Home'],
+      assets: [
+        'static/css/components/Home.css',
+        'static/js/components/Home.js',
+        'static/css/components/Home.css.map',
+        'static/js/components/Home.js.map'
+      ],
+      children: {},
+      childAssets: {}
+    },
+    'components/Home-index': {
+      chunks: ['components/Home'],
+      assets: [
+        'static/css/components/Home.css',
+        'static/js/components/Home.js',
+        'static/css/components/Home.css.map',
+        'static/js/components/Home.js.map'
+      ],
+      children: {},
+      childAssets: {}
+    },
+    'components/Home-index-js': {
+      chunks: ['components/Home'],
+      assets: [
+        'static/css/components/Home.css',
+        'static/js/components/Home.js',
+        'static/css/components/Home.css.map',
+        'static/js/components/Home.js.map'
+      ],
+      children: {},
+      childAssets: {}
+    },
+    'components/NavBar': {
+      chunks: [],
+      assets: [],
+      children: {},
+      childAssets: {}
+    },
+    'components/NavBar-js': {
+      chunks: [],
+      assets: [],
+      children: {},
+      childAssets: {}
+    },
+    'components/Pictures': {
+      chunks: ['components/Pictures'],
+      assets: [
+        'static/js/components/Pictures.js',
+        'static/js/components/Pictures.js.map'
+      ],
+      children: {},
+      childAssets: {}
+    },
+    'components/Pictures-index': {
+      chunks: ['components/Pictures'],
+      assets: [
+        'static/js/components/Pictures.js',
+        'static/js/components/Pictures.js.map'
+      ],
+      children: {},
+      childAssets: {}
+    },
+    'components/Pictures-index-js': {
+      chunks: ['components/Pictures'],
+      assets: [
+        'static/js/components/Pictures.js',
+        'static/js/components/Pictures.js.map'
+      ],
+      children: {},
+      childAssets: {}
+    },
+    'components/Switcher': {
+      chunks: [],
+      assets: [],
+      children: {},
+      childAssets: {}
+    },
+    'components/Switcher-js': {
+      chunks: [],
+      assets: [],
+      children: {},
+      childAssets: {}
+    }
+  }
+}
+
+export default webpackStats
